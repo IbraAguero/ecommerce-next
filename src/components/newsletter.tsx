4d4b7b301d511644f0781ea47b357fa30760a58d
IbraAguero <ibrahimaguero@gmail.com>
2025-02-21 23:52:20 -0300

    feat: add newsletter

---
+++ src/components/newsletter.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { IoIosArrowForward } from "react-icons/io";
+
+function Newsletter() {
+  return (
+    <section className="bg-cyan-800 text-white py-16 sm:py-20">
+      <div className="max-w-xl mx-auto p-5 flex flex-col items-center">
+        <h4 className="text-2xl sm:text-3xl font-semibold tracking-wide">
+          5% OFF | SUSCRIBIENDOTE
+        </h4>
+        <p className="tracking-wider text-zinc-300 text-sm sm:text-base mt-2">
+          Unite al newsletter y recibí tu regalo!
+        </p>
+        <div className="flex gap-1 w-full mt-5">
+          <Input
+            placeholder="Correo electronico"
+            className="placeholder:text-zinc-300 py-6"
+          />
+          <Button size="icon" variant="secondary" className="p-6">
+            <IoIosArrowForward />
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Newsletter;
